8017fd2a130178a9d88e77456ff15979417519a0
Seu Nome <seu@email.com>
2024-08-31 14:49:10 -0300

    ajustes iniciais do front, menu móvel: 	new file:   "desenvolvimento de prot\303\263tipo/img/icone.png" 	new file:   "desenvolvimento de prot\303\263tipo/script.js" 	modified:   "desenvolvimento de prot\303\263tipo/style.css" 	modified:   "desenvolvimento de prot\303\263tipo/teste.html"

---
+++ desenvolvimento de protótipo/script.js
@@ -0,0 +1,33 @@
+const icone = document.getElementById('icone');
+const menu = document.getElementById('menu');
+
+
+
+
+
+icone.addEventListener('click', () => {
+
+    const leftmenu = pxTovh(window.getComputedStyle(menu).left);
+    const value = leftmenu;
+    console.log("teste01")
+    if (value == -34) {
+        menu.style.left = '0vh';
+        console.log("teste02")
+        console.log(value)
+    } else {
+        menu.style.left = '-34vh';
+        console.log("teste03")
+    }
+
+
+
+});
+
+function pxTovh(numpx) {
+
+    const px = parseFloat(numpx);
+
+    return parseFloat(((px / window.innerHeight) * 100).toFixed(2));
+}
+
+
