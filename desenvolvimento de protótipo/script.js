e6533d993dbb7916aa63b7765360a7a6b9efa533
Seu Nome <seu@email.com>
2024-08-31 15:40:30 -0300

    ajustes no front, aparencia da barra de navegação e funcionalidades: 	modified:   "desenvolvimento de prot\303\263tipo/script.js" 	modified:   "desenvolvimento de prot\303\263tipo/style.css" 	modified:   "desenvolvimento de prot\303\263tipo/teste.html"

--- desenvolvimento de protótipo/script.js
+++ desenvolvimento de protótipo/script.js
@@ -10,12 +10,12 @@ icone.addEventListener('click', () => {
     const leftmenu = pxTovh(window.getComputedStyle(menu).left);
     const value = leftmenu;
     console.log("teste01")
-    if (value == -34) {
+    if (value == -42) {
         menu.style.left = '0vh';
         console.log("teste02")
         console.log(value)
     } else {
-        menu.style.left = '-34vh';
+        menu.style.left = '-42vh';
         console.log("teste03")
     }
 
