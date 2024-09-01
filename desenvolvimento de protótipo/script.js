3ce5dde721706c83080ee208c4fc99db91c4a77f
Seu Nome <seu@email.com>
2024-09-01 20:56:54 -0300

    definindo regras para funcionalidades: 	modified:   "desenvolvimento de prot\303\263tipo/img/cabecalhomobile.png" 	modified:   "desenvolvimento de prot\303\263tipo/img/icone.png" 	modified:   "desenvolvimento de prot\303\263tipo/img/imgcabecalho.png" 	modified:   "desenvolvimento de prot\303\263tipo/script.js" 	modified:   "desenvolvimento de prot\303\263tipo/style.css" 	modified:   "desenvolvimento de prot\303\263tipo/teste.html" 	new file:   "desenvolvimento de prot\303\263tipo/transferir()"

--- desenvolvimento de protótipo/script.js
+++ desenvolvimento de protótipo/script.js
@@ -1,8 +1,17 @@
 const icone = document.getElementById('icone');
 const menu = document.getElementById('menu');
-
-
-
+const transferir = document.getElementById('func1');
+const editEestoque = document.getElementById('func1');
+let uso = false;
+transferir.addEventListener('click', () => {
+    const telaTrasf = document.getElementById('transferencia');
+    const teladisplay = window.getComputedStyle(telaTrasf).display;
+    if (teladisplay == 'none') {
+        telaTrasf.style.display = 'flex';
+        menu.style.left = '-42vh';
+        uso = true;
+    }
+})
 
 
 icone.addEventListener('click', () => {
@@ -17,11 +26,19 @@ icone.addEventListener('click', () => {
     } else {
         menu.style.left = '-42vh';
         console.log("teste03")
+    } if (uso == true) {
+        menu.style.left = '-42vh';
+        alerta();
+        console.log("devia fazer o alerta")
     }
 
 
 
 });
+
+function alerta() {
+    alert("é necessário fechar a funcionalidade atua!");
+}
 
 function pxTovh(numpx) {
 
@@ -29,8 +46,18 @@ function pxTovh(numpx) {
 
     return parseFloat(((px / window.innerHeight) * 100).toFixed(2));
 }
+function fechar(button) {
 
-function transferir() {
+    const form = button.closest('form');
+    display = window.getComputedStyle(form).display
+
+    if (display == 'flex') {
+        form.style.display = 'none';
+        uso = false;
+    }
+}
+
+function transfere() {
 
 }
 function editarmz() {
