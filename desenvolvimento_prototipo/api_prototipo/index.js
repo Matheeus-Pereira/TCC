1b5bc3f2f599ad61e5ed3259f4fb86a3710edd47
Seu Nome <seu@email.com>
2024-10-19 09:47:14 -0300

    ajustando endipoints para acssar via front: 	modified:   desenvolvimento_prototipo/api_prototipo/index.js 	modified:   desenvolvimento_prototipo/conection.js

--- desenvolvimento_prototipo/api_prototipo/index.js
+++ desenvolvimento_prototipo/api_prototipo/index.js
@@ -1,19 +1,19 @@
 require("dotenv").config();
  
 const express = require('express');
-const app = express();
+const alicacao = express();
 const port = process.env.PORT;
 const db = require('./db.js');
 
-app.use(express.json());
+alicacao.use(express.json());
  
-app.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
+alicacao.get('/', (req, res) => res.json({ message: 'Funcionando!' }));
 
-app.get('/produtos', async (req, res) => {
+alicacao.get('/produtos', async (req, res) => {
     const results = await db.selectProdutos();
     res.json(results);
 })
 
 //inicia o servidor
-app.listen(port);
+alicacao.listen(port);
 console.log('API funcionando!');
--- desenvolvimento_prototipo/conection.js
+++ desenvolvimento_prototipo/conection.js
@@ -1,20 +1,62 @@
 
 const express = require('express');
+const mysql = require('mysql2/promise')
+const { transfItem, searchEstoque } = require('./db.js')
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res)=>res.json({
-    message:'funcionando'
+app.get('/', (req, res) => res.json({
+    message: 'funcionando'
 }))
 
-
+const connection = mysql.createPool(process.env.CONNECTION_STRING)
 
 // -------------------------  ENDPOINTS -------------------------
 
-app.post('/transfere:', (req, res)=>{
-    const
+app.post('/transfere', async (req, res) => {
+    const { idA, origem, destino, quant } = req.body
+
+    if (!idA || !origem || !destino || !quant) {
+        return res.status(400).json({ error: 'parametros inválidos' });
+    }
+
+    try {
+        await transfItem(idA, origem, destino, quant);
+        res.status(200).json({ message: 'transferencia realizada com sucesso' });
+    } catch (error) {
+        console.error('erro ao transferir item:', error)
+        res.status(500).json({ error: 'erro ao tranferir item' })
+    }
+
+});
+
+
+app.get('/estoques', async (req, res) => {
+    try {
+        const sql = 'select * from estoques';
+        const [results] = await connection.query(sql);
+
+        if (results.length === 0) {
+            return res.status(404).json({ message: 'nenhum estoque encontrado' });
+        }
+
+        res.status(200).json(results);
+
+    } catch (error) {
+        console.error('erro ao consultar estoques:', error);
+        res.status(500).json({ error: 'erro ao consultar estoques' })
+    }
 })
+
+
+// -------------------------  INICIA SERVIDOR -------------------------
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`porta do server:${PORT}`)
+});
 
 
 // app.post('/transferir-item', (req, res) => {
@@ -31,8 +73,5 @@ app.post('/transfere:', (req, res)=>{
 // })
 
 
-// -------------------------  INICIA SERVIDOR -------------------------
 
-app.listen(3000, () => {
-    console.log('servidor rodando,porta 3000')
-})
+
